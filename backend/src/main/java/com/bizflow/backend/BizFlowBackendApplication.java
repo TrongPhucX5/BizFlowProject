b85cfcc6dcbd69c14dd63121b235be6804731a88
TrongPhucX5 <lphuc2324@gmail.com>
2025-12-08 15:56:50 +0700

    Khởi tạo cấu trúc dự án: SpringBoot, Flutter, NextJS

---
+++ backend/src/main/java/com/bizflow/backend/BizFlowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bizflow.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizFlowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizFlowBackendApplication.class, args);
+	}
+
+}
